f3219500812adc7e919f429bc95259064e6b5179
AngelRosin16 <jojedaplas@gmail.com>
2025-04-24 21:55:39 +0800

    naraaaa

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 class CSVManager:
-    """Complete this class"""
-    def addValue(value):
-        """Complete this method
-        Receive value(user input) and store into csv file"""
-        pass
-
-    def showAll():
-        """Complete this method
-        Show all values in storage in a numbered format"""
-        pass
-
-if __name__=="__main__":
-    manager= CSVManager()
-
+    #example onlyy
